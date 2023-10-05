6a8c1401b9d08d0098852e90a486bfa1aa5f54dd
Kripther <119134408+Kripther@users.noreply.github.com>
2023-10-04 22:19:22 -0600

    Numeros ahora sí
    Me faltaba un corchete que borre junto con un comentario al final que por eso no probé y me di cuenta hasta ahorita, un saludo

--- Scanner.java
+++ Scanner.java
@@ -22,6 +22,7 @@ public class Scanner {
         palabrasReservadas.put("true",   TipoToken.TRUE);
         palabrasReservadas.put("var",    TipoToken.VAR);
         palabrasReservadas.put("while",  TipoToken.WHILE);
+
     }
 
     private final String source;
@@ -33,8 +34,11 @@ public class Scanner {
     }
 
     public List<Token> scan() throws Exception {
-        int estado = 0;
         String lexema = "";
+        String numero = ""; //variables para convertir un numero en texto a un numero en doble  
+        String exponente ="";
+        String signo = "";
+        int estado = 0;
         char c;
 
         for(int i=0; i<source.length(); i++){
@@ -49,20 +53,8 @@ public class Scanner {
                     else if(Character.isDigit(c)){
                         estado = 15;
                         lexema += c;
-
-                        /*while(Character.isDigit(c)){
-                            lexema += c;
-                            i++;
-                            c = source.charAt(i);
-                        }
-                        Token t = new Token(TipoToken.NUMBER, lexema, Integer.valueOf(lexema));
-                        lexema = "";
-                        estado = 0;
-                        tokens.add(t);
-                        */
-
                     }
-                    break;
+                break;
 
                 case 13:
                     if(Character.isLetterOrDigit(c)){
@@ -87,24 +79,121 @@ public class Scanner {
 
                     }
                     break;
-
+                //caso para generar el token de los numeros 
                 case 15:
+                    //esto va guardando el numero saliendo del caso y volviendo a entrar caracter a caracter
+                    //para mantener el formato del profe 
                     if(Character.isDigit(c)){
                         estado = 15;
                         lexema += c;
                     }
+                    //esta parte considera que ya se leyo toda la parte entera y empieza la decimal
                     else if(c == '.'){
+                        lexema += c;
 
+                        i++;
+                        c = source.charAt(i);
+                        while(i < source.length()-1 && Character.isDigit(c) ){//esto lee toda la parte decimal
+                            lexema += c;
+                            i++;
+                            c = source.charAt(i);
+                        }
+                        numero = lexema;//se guarda el numero como flotante a modo de cadena
+                        if(source.charAt(i) == 'E'){//empieza la parte exponencial 
+                            lexema += c; 
+                            i++;
+                            c = source.charAt(i);
+                            if(Character.isDigit(c)){//identifica si el exponente tiene signo o no
+                                lexema += c;
+                                exponente += c;
+                                i++;
+                                c = source.charAt(i);
+                                while(i < source.length()-1 && Character.isDigit(c) ){//mientras se lea un digito y no termine el archivo
+                                    lexema += c;
+                                    exponente += c;
+                                    i++;
+                                    c = source.charAt(i);
+                                }
+                            }else if (c == '+' || c == '-'){
+                                lexema += c;
+                                signo += c;
+                                i++;
+                                c = source.charAt(i);
+                                while(i < source.length()-1 && Character.isDigit(c) ){
+                                    lexema += c;
+                                    exponente += c;
+                                    i++;
+                                    c = source.charAt(i);
+                                }
+                            }
+                            //genera el token de numero y convierte el texto a numero
+                            //problema para manejar exponentes grandes 
+                            Token t69 = new Token(TipoToken.NUMBER, lexema,Float.parseFloat(numero)* Math.pow(10,Float.parseFloat(signo+exponente)) );
+                            tokens.add(t69);
+                            estado=0;
+                            exponente = "";
+                            numero= "";
+                            lexema ="";
+                            signo ="";
+                            i--;
+                        }
+                        else{//caso para manejar numero con punto decimal sin exponente
+                            Token t6 = new Token(TipoToken.NUMBER, lexema,Float.parseFloat(lexema+"0") );//se agrega el 0 para considerar los casos donde se recibe "numero."
+                            tokens.add(t6);
+                            estado=0;
+                            exponente = "";
+                            numero= "";
+                            lexema ="";
+                            signo ="";
+                            i--;
+                        }
                     }
-                    else if(c == 'E'){
-
+                    else if(c == 'E'){//caso para manejar numero entero con exponente
+                        numero = lexema;
+                        lexema += c; 
+                        i++;
+                        c = source.charAt(i);
+                        if(Character.isDigit(c)){
+                                lexema += c;
+                                exponente += c;
+                                i++;
+                                c = source.charAt(i);
+                                while(i < source.length()-1 && Character.isDigit(c) ){
+                                    lexema += c;
+                                    exponente += c;
+                                    i++;
+                                    c = source.charAt(i);
+                                }
+                            }else if (c == '+' || c == '-'){
+                                lexema += c;
+                                signo += c;
+                                i++;
+                                c = source.charAt(i);
+                                while(i < source.length()-1 && Character.isDigit(c) ){
+                                    lexema += c;
+                                    exponente += c;
+                                    i++;
+                                    c = source.charAt(i);
+                                }
+                            }
+                        Token t69 = new Token(TipoToken.NUMBER, lexema, Integer.valueOf(numero)* Math.pow(10,Float.parseFloat(signo+exponente)));
+                        tokens.add(t69);
+                        estado=0;
+                        exponente = "";
+                        numero= "";
+                        lexema ="";
+                        signo ="";
+                        i--;
                     }
-                    else{
-                        Token t = new Token(TipoToken.NUMBER, lexema, Integer.valueOf(lexema));
-                        tokens.add(t);
+                    else{//caso para manejar numero enteros sin exponente
+                        Token t7 = new Token(TipoToken.NUMBER, lexema, Integer.valueOf(lexema));
+                        tokens.add(t7);
 
-                        estado = 0;
-                        lexema = "";
+                        estado=0;
+                        exponente = "";
+                        numero= "";
+                        lexema ="";
+                        signo ="";
                         i--;
                     }
                     break;
